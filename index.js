db3e18be276d161d822cd2ff8b1020e4f7e66741
rucha.bb9373 <rucha.zirpe@firstcry.com>
2025-11-28 23:21:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// backend/index.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/ping', (req, res) => res.json({ ok: true, message: 'pong' }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server listening ${PORT}`));
